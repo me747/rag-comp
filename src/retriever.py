e5aada8a247f886f9f3aaf8800ee4cd4c1faf423
Aftaab <aftaab21@gmail.com>
2026-07-22 21:09:45 -0400

    added a new test case to figure out why LLM failed on q4

--- src/retriever.py
+++ src/retriever.py
@@ -1,12 +1,12 @@
 # putting chunking, embedding & storage into a single thing that i can call
 
-from data_loader import load_pdf
-from chunker import chunk_text
-from embedder import embed_text
-from vec_storage import VecStorage
+from .data_loader import load_pdf,load_txt
+from .chunker import chunk_text
+from .embedder import embed_text
+from .vec_storage import VecStorage
 
-def build_idx(pdf_path):
-    text = load_pdf(pdf_path)
+def build_idx(txt_path): # decided to prototype on public domain so changing pdf to txt doc
+    text = load_txt(txt_path)
     chunks = chunk_text(text)
 
     print(f"building an index from {len(chunks)} chunks, might take a sec")
@@ -23,12 +23,26 @@ def retrieve(store, question, k=5):
     return results
 
 if __name__ == '__main__':
-    store = build_idx("data/10050-medicare-and-you.pdf")
-    question = "What does Medicare Part B cover"
-    results = retrieve(store, question)
-    print(f"question: {question}\n")
+ 
+    store = build_idx("data/the_odyssey.txt") # changed test case to match public domain use
+    
+   # question = "Who is Odysseus's wife?"
+   # results = retrieve(store, question)
+   # print(f"question: {question}\n")
+   # for i, r in enumerate(results):
+   #     print(f"***result {i+1}***")
+   #     print(f"{r}\n")
+    
+    '''
+    added this test case after running the baseline, for q4 the LLM was unable to answer this even though Telemachus is constantly mentioned acros the narration 
+    & the model answering there is no mention of Odysseus's own son" almost certainly seems like a retrieval failure, not a generation failure, so doing a manual quick check
+    '''
+    test_question = "Who is Odysseus's son?"
+    results = retrieve(store, test_question)
+    print(f"question: {test_question}\n")
     for i, r in enumerate(results):
         print(f"***result {i+1}***")
         print(f"{r}\n")
+    
         
 
